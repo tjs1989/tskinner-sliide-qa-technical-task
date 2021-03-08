037d0169d5066dd83553afb5b386ea8b429e2e26
tony <tony@thoughtmachine.net>
2021-03-08 15:16:14 +0000

    Added wait for resource mechanism

--- app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/news/LoginInstrumentedTest.kt
@@ -1,31 +1,43 @@
+package com.test.news.features.login.presentation
+
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.clearText
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.test.news.R
-import com.test.news.features.login.presentation.LoginActivity
-import junit.framework.Assert.assertTrue
+import com.test.news.userInterations.clearAndEnterTextInField
+import com.test.news.userInterations.tapOnButton
+import com.test.news.rules.AnimationsRule
+import com.test.news.viewActions.ViewActions.waitForResourceToDisplay
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class LoginInstrumentedTest {
 
     @get:Rule
     var activityTestRule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
 
+    @get:Rule
+    val animationsRule = AnimationsRule()
+
     @Test
     fun shouldLoginWithValidCredentials() {
-        onView(withId(R.id.editTextUserName))
-            .perform(clearText(), typeText(VALID_USER_NAME))
-        onView(withId(R.id.editTextPassword))
-            .perform(clearText(), typeText(VALID_USER_PASSWORD))
-        onView(withId(R.id.buttonLogin))
-            .perform(click())
+        clearAndEnterTextInField(
+            R.id.editTextUserName,
+            VALID_USER_NAME
+        )
+        clearAndEnterTextInField(
+            R.id.editTextPassword,
+            VALID_USER_PASSWORD
+        )
+        tapOnButton(R.id.buttonLogin)
+
 
         // TODO assert login when ready
-        assertTrue(activityTestRule.activity.isFinishing)
+//        assertTrue(activityTestRule.activity.isFinishing)
+        onView(isRoot()).perform(waitForResourceToDisplay(R.id.imageView, 5000));
     }
 
     companion object {
